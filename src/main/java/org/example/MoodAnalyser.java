24b19d56bcc0a8265e810f39ecb0732500a631ea
Hari Naik <kumavathharinaik@gmail.com>
2024-07-11 13:14:45 +0530

    UC1 : Happy Sad

--- src/main/java/org/example/MoodAnalyser.java
+++ src/main/java/org/example/MoodAnalyser.java
@@ -1,7 +1,22 @@
 package org.example;
 
 public class MoodAnalyser {
+    public static String analyseMood(String message){
+        if (message.contains("Sad")){
+            return "Sad";
+        }
+        return "Happy";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Mood Analyser Project");
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+        String message1 = "I am Sad";
+        String mood1 = MoodAnalyser.analyseMood(message1);
+        System.out.println("Test case 1.1 : "+mood1);
+
+        String message2 = "I am Happy";
+        String mood2 = MoodAnalyser.analyseMood(message2);
+        System.out.println("Test case 1.2 : "+mood2);
     }
 }
